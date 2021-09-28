06a6bcf9bedcbc8e02cc6631f128854a33604691
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-28 22:22:25 +0600

    Programe has been completed...

--- EnumExample/Program.cs
+++ EnumExample/Program.cs
@@ -22,16 +22,16 @@ namespace EnumExample
 
             //access the Enum
 
-            Console.WriteLine(weekDays.Monday);
-            Console.WriteLine(weekDays.Tuesday);
-            Console.WriteLine(weekDays.Wednesday);
-            Console.WriteLine(weekDays.Thursday);
-            Console.WriteLine(weekDays.Friday);
-            Console.WriteLine(weekDays.Saturday);
-            Console.WriteLine(weekDays.Sunday);
+            Console.WriteLine(weekDays.Monday); //Monday
+            Console.WriteLine(weekDays.Tuesday); //Tuesday
+            Console.WriteLine(weekDays.Wednesday); //Wednesday
+            Console.WriteLine(weekDays.Thursday); // Thursday
+            Console.WriteLine(weekDays.Friday); // Friday
+            Console.WriteLine(weekDays.Saturday); // Saturday
+            Console.WriteLine(weekDays.Sunday); // Sunday
 
 
 
         }
-}
+    }
 }
