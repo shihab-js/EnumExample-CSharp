f40989c55a64aa999ff825706ba3dc2f30081a70
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-28 22:20:25 +0600

    Enum access successfuly...

--- EnumExample/Program.cs
+++ EnumExample/Program.cs
@@ -19,9 +19,19 @@ namespace EnumExample
 
         static void Main(string[] args)
         {
-            
-            
-        
-    }
+
+            //access the Enum
+
+            Console.WriteLine(weekDays.Monday);
+            Console.WriteLine(weekDays.Tuesday);
+            Console.WriteLine(weekDays.Wednesday);
+            Console.WriteLine(weekDays.Thursday);
+            Console.WriteLine(weekDays.Friday);
+            Console.WriteLine(weekDays.Saturday);
+            Console.WriteLine(weekDays.Sunday);
+
+
+
+        }
 }
 }
