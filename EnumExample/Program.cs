8440d5e678d52747c50c249a2fe01852d8e796e7
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-28 22:17:12 +0600

    Enum has  been created...

--- EnumExample/Program.cs
+++ EnumExample/Program.cs
@@ -4,9 +4,24 @@ namespace EnumExample
 {
     class Program
     {
+        enum weekDays
+        {
+            //create a enum
+
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
         static void Main(string[] args)
         {
             
-        }
+            
+        
     }
+}
 }
